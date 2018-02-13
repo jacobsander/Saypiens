7bde571425a4e5708e110439e9fbeebb941fa282
Jacob Sander <jacobsander92@gmail.com>
2018-02-13 23:24:49 +0100

    v5
    change sidebar to cirlces and adjusted css for mailchimp embed

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,6 +1,123 @@
-// smooth scroll
+$('body').scrollspy({ target: '.sidenav' });
+$('ul.nav li a').bind('click', function(e) {
+  e.preventDefault();
+  $('html,body').animate({scrollTop: $(this.hash).offset().top});
+});
+
+
+// smooth scroll for join button
 
 $(".nav a[href^='#']").on('click', function(e) {
+
+   // prevent default anchor click behavior
+   e.preventDefault();
+
+   // store hash
+   var hash = this.hash;
+
+   // animate
+   $('html, body').animate({
+       scrollTop: $(hash).offset().top
+     }, 600, function(){
+
+       // when done, add hash to url
+       // (default click behaviour)
+       window.location.hash = hash;
+     });
+
+});
+
+// smooth scroll for sidebar-1 button
+
+$("#sidebar-1").on('click', function(e) {
+
+   // prevent default anchor click behavior
+   e.preventDefault();
+
+   // store hash
+   var hash = this.hash;
+
+   // animate
+   $('html, body').animate({
+       scrollTop: $(hash).offset().top
+     }, 600, function(){
+
+       // when done, add hash to url
+       // (default click behaviour)
+       window.location.hash = hash;
+     });
+
+});
+
+// smooth scroll for sidebar-2 button
+
+$("#sidebar-2").on('click', function(e) {
+
+   // prevent default anchor click behavior
+   e.preventDefault();
+
+   // store hash
+   var hash = this.hash;
+
+   // animate
+   $('html, body').animate({
+       scrollTop: $(hash).offset().top
+     }, 600, function(){
+
+       // when done, add hash to url
+       // (default click behaviour)
+       window.location.hash = hash;
+     });
+
+});
+
+// smooth scroll for sidebar-3 button
+
+$("#sidebar-3").on('click', function(e) {
+
+   // prevent default anchor click behavior
+   e.preventDefault();
+
+   // store hash
+   var hash = this.hash;
+
+   // animate
+   $('html, body').animate({
+       scrollTop: $(hash).offset().top
+     }, 600, function(){
+
+       // when done, add hash to url
+       // (default click behaviour)
+       window.location.hash = hash;
+     });
+
+});
+
+// smooth scroll for sidebar-4 button
+
+$("#sidebar-4").on('click', function(e) {
+
+   // prevent default anchor click behavior
+   e.preventDefault();
+
+   // store hash
+   var hash = this.hash;
+
+   // animate
+   $('html, body').animate({
+       scrollTop: $(hash).offset().top
+     }, 600, function(){
+
+       // when done, add hash to url
+       // (default click behaviour)
+       window.location.hash = hash;
+     });
+
+});
+
+// smooth scroll for sidebar-5 button
+
+$("#sidebar-5").on('click', function(e) {
 
    // prevent default anchor click behavior
    e.preventDefault();
